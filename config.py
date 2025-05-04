642266c71669c1e7f22e33b833dc34fa50e6db21
EdenSwack <93931254+EdenSwack@users.noreply.github.com>
2025-05-04 10:09:39 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-TINY_USERNAME = "Q4israel"
+TINY_USERNAME = ""
 BASE_URL = "https://tiny.cc/tiny/api/3/"
 
-BQ_PROJECT_ID = "translation-429608"
-BQ_DATASET_ID = "other_data"
-BQ_TABLE_ID = "tiny_CC_data"
+BQ_PROJECT_ID = ""
+BQ_DATASET_ID = ""
+BQ_TABLE_ID = ""
